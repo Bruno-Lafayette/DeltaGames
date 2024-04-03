a91e5a6c243b6c798a0daa404370f3966ed924a9
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-04-02 22:07:22 -0300

    feat: Add Material na tela Login

--- app/src/main/java/com/example/deltagames/view/ProfileScreen/components/TabItem.kt
+++ app/src/main/java/com/example/deltagames/view/ProfileScreen/components/TabItem.kt
@@ -1,5 +1,6 @@
 package com.example.deltagames.view.ProfileScreen.components
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import com.example.deltagames.view.loginScreen.LoginScreen
 import com.example.deltagames.view.RegisterScreen.RegisterScreen
@@ -7,6 +8,7 @@ import com.example.deltagames.view.RegisterScreen.RegisterScreen
 typealias ComposableFun = @Composable ()->Unit
 
 sealed class TabItem (val title: String, val labelButton: String, val screens: ComposableFun ){
+    @OptIn(ExperimentalMaterial3Api::class)
     object Login: TabItem(
         title = "Entrar na sua conta",
         labelButton = "Entrar",
